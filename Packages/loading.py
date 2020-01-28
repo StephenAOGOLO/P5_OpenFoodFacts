6ff59f847a3951b3eca76c4ccc450552f0df2e93
stephenAOGOLO <stephen.adams.ogolo@gmail.com>
2020-01-28 16:09:08 +0100

    3rd commit - on PythonBddApi. -rebuild project P5

---
+++ Packages/loading.py
@@ -0,0 +1,14 @@
+"""
+Welcome to the loading module, 'loading.py'.
+This module is especially composed of one function.
+Which is used to initialize and feeds program parameters.
+"""
+# -*- coding: utf-8 -*-
+import configparser
+
+
+def initialization():
+    """
+    So this function is ran to concentrate these parameters first and return dict ready to use.
+    """
+    pass
---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Welcome to the main program 'main.py'.
+When it starts the program process is following the steps below:
+- The initialization
+- The provided parameters
+- The player management
+- The endgame:
+"""
+# -*- coding: utf-8 -*-
+
