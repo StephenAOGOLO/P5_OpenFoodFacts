b66243707c6a0965a5b5d7c5a71ace66adc3ff94
stephenAOGOLO <stephen.adams.ogolo@gmail.com>
2020-02-11 13:54:54 +0100

    14th commit - on PythonBddApi. - Adding data to ALIMENT TABLE

--- test.py
+++ test.py
@@ -18,10 +18,6 @@ import logging as lg
 lg.basicConfig(level=lg.WARNING)
 
 
-def close_connection():
-    cnx.close
-
-
 def main_off():
 
     cnx = mc.connect(
@@ -50,7 +46,7 @@ def open_json_file(file):
 
 def load_api_data():
     dict_data = {}
-    list_c = ["product_name", "categories", "brands", "nutriscore_grade", "stores", "purchase_places", "url"]
+    list_c = ["product_name", "brands", "nutriscore_grade", "stores", "purchase_places", "url"]
     dict_data["sent"] = {}
     dict_data["rcvd"] = {}
     dict_data["sent"]["urls"] = open_json_file(".\\Packages\\urls.json")
@@ -68,16 +64,29 @@ def load_api_data():
                     dict_data["rcvd"]["aliments"][url_name][str(i)][element] = dict_data["rcvd"][url_name]["products"][i][element]
                 else:
                     dict_data["rcvd"]["aliments"][url_name][str(i)][element] = "NOT_PROVIDED"
-
-    dict_data = prepare_sql_values(dict_data)
+    dict_data = all_rows(dict_data)
     dict_data = all_categories(dict_data)
+    dict_data = prepare_sql_values(dict_data)
     return dict_data
 
 
 def all_categories(all_data):
-    all_data["rcvd"]["categories"]=[]
+    all_data["rcvd"]["local_category"]=[]
     for key in all_data["rcvd"]["aliments"].keys():
-        all_data["rcvd"]["categories"].append(key)
+        all_data["rcvd"]["local_category"].append(key)
+    return all_data
+
+
+def all_rows(all_data):
+
+    all_data["rcvd"]["rows"] = ["product_name",
+                                "local_category",
+                                "brands",
+                                "nutriscore_grade",
+                                "stores",
+                                "purchase_places",
+                                "url"
+                                ]
     return all_data
 
 
@@ -90,7 +99,17 @@ def prepare_sql_values(all_data):
     for category in list_categories:
         for i in range(0, 20):
             all_data["rcvd"]["sql_values"][category][category+"_"+str(i)] = all_data["rcvd"]["aliments"][category][str(i)]
-    for k, v in all_data["rcvd"]["sql_values"]["beurre"].items  ():
+    for category in list_categories:
+        for i in range(0, 20):
+            for row in all_data["rcvd"]["rows"]:
+                try:
+                    the_row = all_data["rcvd"]["sql_values"][category][category+"_"+str(i)][row]
+                    the_row = str(the_row).replace(","," ou ")
+                    the_row = the_row.replace("'","-")
+                    all_data["rcvd"]["sql_values"][category][category + "_" + str(i)][row] = the_row
+                except Exception as e:
+                    lg.info("erreur sur part - ", e)
+    for k, v in all_data["rcvd"]["sql_values"]["beurre"].items():
         lg.info("{} -> {}".format(k, v))
     return all_data
 
@@ -116,34 +135,54 @@ def display_sorted_results(all_data):
     print("-" * 20)
 
 
+def fill_table_category():
+    ## TABLE CATEGORY
+    session = mo.Mysql("stephen", "stephen")
+    dico = load_api_data()
+    for i, category in enumerate(dico["rcvd"]["local_category"]):
+        pure_value = "('{}', '{}')".format(i+1, category)
+        session.insert_data("category", "(id, name)", pure_value)
+
+
+def fill_table_aliment():
+
+    # TABLE ALIMENT
+    session = mo.Mysql("stephen", "stephen")
+    dico = load_api_data()
+    for category in dico["rcvd"]["local_category"]:
+        for i in range(0, 20):
+            raw_data = ""
+            dico["rcvd"]["sql_values"][category + "_" + str(i)] = {}
+            dico["rcvd"]["sql_values"][category + "_" + str(i)]["pure_data"] = []
+            dico["rcvd"]["sql_values"][category + "_" + str(i)]["raw_data"] = ""
+            i_rows = 0
+            for k, v in dico["rcvd"]["sql_values"][category][category+"_"+str(i)].items():
+                dico["rcvd"]["sql_values"][category + "_" + str(i)]["pure_data"].append(v)
+                if i_rows == 0:
+                    raw_data += v
+                else:
+                    raw_data += ", " + v
+                i_rows += 1
+            dico["rcvd"]["sql_values"][category + "_" + str(i)]["raw_data"] = raw_data
+            raw_data = ""
+    for i in range(0, 20):
+        for category in dico["rcvd"]["local_category"]:
+            value = dico["rcvd"]["sql_values"][category + "_" + str(i)]["raw_data"]
+            value += ","+category
+            value += "," + category + "_" + str(i)
+            value = value.replace(",", "' , '")
+            pure_value = "('{}')".format(value)
+            session.insert_data("aliment", "(product_name, brands, nutriscore_grade, stores, purchase_places, url, local_category, local_name)", pure_value)
+
+
 def create_products(all_data):
 
     pass
 
 
 if __name__ == "__main__":
-    # TABLE CATEGORY
-    session = mo.Mysql("stephen", "stephen")
-    dico = load_api_data()
-    for i, category in enumerate(dico["rcvd"]["categories"]):
-        pure_value = "('{}', '{}')".format(i+1, category)
-        session.insert_data("category", "(id, name)", pure_value)
-
-    # TABLE ALIMENT
-    #session = mo.Mysql("stephen", "stephen")
-    #dico = load_api_data()
-    #list_values = []
-    #for category in dico["rcvd"]["categories"]:
-    #    list_values.append()
-    #    #i = 0
-    #    #for row, value in dico["rcvd"]["categories"][category][category+"_"+str(i)].items():
-    #    #    pure_value = "('{}', '{}')".format(i+1, category)
-    #    #    session.insert_data("aliment", "(product_name, categories, brands, nutriscore_grade, stores, purchase_places, url)", pure_value)
-
-
-
-
-
+    fill_table_category()
+    fill_table_aliment()
     #session = mo.Mysql("stephen", "stephen")
     #session.show_db()
     #session.show_tbs()
