e6bdfb06792d7e184386bdea891ee2fbcb94fc9e
stephenAOGOLO <stephen.adams.ogolo@gmail.com>
2020-02-14 14:10:21 +0100

    24th commit - on PythonBddApi. - Adding creation of enitre db_purebeurre

--- Packages/api_operations.py
+++ Packages/api_operations.py
@@ -15,11 +15,14 @@ def load_api_data():
     dict_data["sent"] = {}
     dict_data["rcvd"] = {}
     dict_data["sent"]["urls"] = open_json_file(".\\Packages\\urls.json")
+    #dict_data["sent"]["urls"] = open_json_file("urls.json")
     for url_name, url in dict_data["sent"]["urls"].items():
+        print("Retrieving data from OpenFoodFacts server in progress...")
         response = requests.get(url)
         dict_data["rcvd"][url_name] = response.json()
+        print("Data received from OpenFoodFacts server")
+        print("Data organizing in progress...")
     dict_data["rcvd"]["aliments"] = {}
-
     for url_name, url in dict_data["sent"]["urls"].items():
         dict_data["rcvd"]["aliments"][url_name] = {}
         for i in range(0, 20):
@@ -29,14 +32,17 @@ def load_api_data():
                     dict_data["rcvd"]["aliments"][url_name][str(i)][element] = dict_data["rcvd"][url_name]["products"][i][element]
                 else:
                     dict_data["rcvd"]["aliments"][url_name][str(i)][element] = "NOT_PROVIDED"
+    print("Getting data ready for console and local database...")
     dict_data = all_rows(dict_data)
     dict_data = all_categories(dict_data)
     dict_data = prepare_sql_values(dict_data)
+    print("Data ready!!!")
+    print("Data initialization complete.")
     return dict_data
 
 
 def all_categories(all_data):
-    all_data["rcvd"]["local_category"]=[]
+    all_data["rcvd"]["local_category"] = []
     for key in all_data["rcvd"]["aliments"].keys():
         all_data["rcvd"]["local_category"].append(key)
     return all_data
@@ -53,6 +59,37 @@ def all_rows(all_data):
                                 "url"
                                 ]
     return all_data
+
+
+def provide_categories(all_data):
+
+    all_data["console"]["categories"] = {}
+    for i, element in enumerate(all_data["rcvd"]["local_category"]):
+        lg.info("{} - {}".format(i, element))
+        all_data["console"]["categories"][i] = element
+    return all_data["console"]["categories"]
+
+
+def console_data(all_data):
+    all_data["console"] = {}
+    provide_categories(all_data)
+    return all_data["console"]
+
+
+def show_all_data(all_data):
+    if type(all_data) == dict:
+        for k, v in all_data.items():
+            print("{} => {}".format(k, v))
+            print("*"*50)
+            print("\n")
+            if type(v) == dict:
+                show_all_data(v)
+    else:
+        print("*"*50)
+        print("Fonction show_all_data(element)")
+        print("element n'est pas un dict...")
+        print("*" * 50)
+    return 0
 
 
 def prepare_sql_values(all_data):
@@ -77,3 +114,11 @@ def prepare_sql_values(all_data):
     for k, v in all_data["rcvd"]["sql_values"]["beurre"].items():
         lg.info("{} -> {}".format(k, v))
     return all_data
+
+
+if __name__ =="__main__":
+    dico = load_api_data()
+    categ = provide_categories(dico)
+    for k, v in categ.items():
+        print("{} - {}".format(k, v))
+    show_all_data(dico["rcvd"]["local_category"])
--- Packages/loading.py
+++ Packages/loading.py
@@ -9,10 +9,12 @@ import os
 import time
 import psutil
 import logging as lg
+import Packages.mysql_operations as mo
+import Packages.api_operations as ao
 lg.basicConfig(level=lg.WARNING)
 
 
-def initialization():
+def off_initialization():
     """
     So this function is ran to concentrate these parameters first and return dict ready to use.
     """
@@ -28,6 +30,83 @@ def initialization():
             root_status = True
             break
     return root_status
+
+
+def initialization():
+    create_db_purebeurre()
+    #big_data = ao.load_api_data()
+    #fill_table_category(big_data)
+    #fill_table_aliment(big_data)
+
+
+def create_db_purebeurre():
+    session = mo.Mysql("stephen", "stephen")
+    session.create_db()
+    session = mo.Mysql("stephen", "stephen", "db_purebeurre")
+    contenu = open_sql_file("test.sql")
+    contenu = "".join(contenu)
+    contenu = contenu.replace("\n","")
+    contenu = contenu.split(";")
+    dict_tables = {"category":contenu[0],"aliment":contenu[1],"historic":contenu[2]}
+    create_tables(dict_tables)
+
+
+def create_tables(tables_data):
+    session = mo.Mysql("stephen", "stephen", "db_purebeurre")
+    for table, data in tables_data.items():
+        session.executing(data)
+    return True
+
+
+def create_table_category():
+    pass
+
+
+def create_table_aliment():
+    pass
+
+
+def create_table_historic():
+    pass
+
+def fill_table_category(dico):
+    ## TABLE CATEGORY
+    session = mo.Mysql("stephen", "stephen")
+    #dico = ao.load_api_data()
+    for i, category in enumerate(dico["rcvd"]["local_category"]):
+        pure_value = "('{}', '{}')".format(i+1, category)
+        session.insert_data("category", "(id, name)", pure_value)
+
+
+def fill_table_aliment(dico):
+
+    # TABLE ALIMENT
+    session = mo.Mysql("stephen", "stephen")
+    #dico = ao.load_api_data()
+    for category in dico["rcvd"]["local_category"]:
+        for i in range(0, 20):
+            raw_data = ""
+            dico["rcvd"]["sql_values"][category + "_" + str(i)] = {}
+            dico["rcvd"]["sql_values"][category + "_" + str(i)]["pure_data"] = []
+            dico["rcvd"]["sql_values"][category + "_" + str(i)]["raw_data"] = ""
+            i_rows = 0
+            for k, v in dico["rcvd"]["sql_values"][category][category+"_"+str(i)].items():
+                dico["rcvd"]["sql_values"][category + "_" + str(i)]["pure_data"].append(v)
+                if i_rows == 0:
+                    raw_data += v
+                else:
+                    raw_data += ", " + v
+                i_rows += 1
+            dico["rcvd"]["sql_values"][category + "_" + str(i)]["raw_data"] = raw_data
+            raw_data = ""
+    for i in range(0, 20):
+        for category in dico["rcvd"]["local_category"]:
+            value = dico["rcvd"]["sql_values"][category + "_" + str(i)]["raw_data"]
+            value += ","+category
+            value += "," + category + "_" + str(i)
+            value = value.replace(",", "' , '")
+            pure_value = "('{}')".format(value)
+            session.insert_data("aliment", "(product_name, brands, nutriscore_grade, stores, purchase_places, url, local_category, local_name)", pure_value)
 
 
 def find_process(the_process="le_program_executable.exe"):
@@ -48,7 +127,42 @@ def find_process(the_process="le_program_executable.exe"):
     return dico_processus, status
 
 
+def open_sql_file(path_fichier):
+    """ Fonction d'ouverture d'un fichier
+                        et
+        sauvegrade du contenu en mémoire
+
+    :param path_fichier:
+    :return liste_fichier: """
+
+    with open(path_fichier,"rt") as fichier:
+        liste_fichier = fichier.readlines()
+    print("=" * 150)
+    print("\nVoici le contenu du fichier : {}\n".format(path_fichier))
+    for indice, ligne in enumerate(liste_fichier):
+        print("ligne {} : {}".format(indice, ligne))
+    print("=" * 150)
+    print("\nFin de fichier\n")
+    print("=" * 150)
+    return liste_fichier
+
 if __name__ == "__main__":
     initialization()
+    #contenu = open_sql_file("test.sql")
+    #contenu = "".join(contenu)
+    #contenu = contenu.replace("\n","")
+    #contenu = contenu.split(";")
+    #for e in contenu:
+    #    print(e)
+    #dico_tables = {}
+    #dico_tables["category"] = contenu[0]
+    #dico_tables["aliment"] = contenu[1]
+    #dico_tables["historic"] = contenu[2]
+    #print("\n")
+    #for k, v in dico_tables.items():
+    #    print(k, v)
+
+
+
 
 
